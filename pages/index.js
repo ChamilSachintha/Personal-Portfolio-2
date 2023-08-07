0b601a1e4da2d9f25215020bd0b0e5bf0bad79f6
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2023-08-07 21:45:32 +0530

    Avatar and background images were added

--- pages/index.js
+++ pages/index.js
@@ -13,12 +13,13 @@ const Home = () => {
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           {/* Heading */}
-          <motion.h1 
-          variants={fadeIn("down", 0.2)}
+          <motion.h1
+            variants={fadeIn("down", 0.2)}
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="h1">
+            className="h1"
+          >
             Hi, I'm Chamil Sachintha <br />{" "}
             <span className="text-accent">
               Full-Stack, Front-End & UI/UX Engineer
@@ -32,7 +33,7 @@ const Home = () => {
             exit="hidden"
             className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
           >
-            I am passionate about developing full-stack applications, front-end
+            I am passionate about developing Full-Stack applications, Front-End
             applications and designing UI/UX and have a good point of view on
             colors.
           </motion.p>
@@ -49,6 +50,17 @@ const Home = () => {
           >
             <ProjectsBtn />
           </motion.div>
+        </div>
+      </div>
+      {/* Image */}
+      <div className="w-[1200px] h-full absolute right-0 bottom-0">
+      {/* Background image */}
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+        {/* Particles */}
+        <div>Particles</div>
+        {/* Avatar image */}
+        <div className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]">
+          <Avatar />
         </div>
       </div>
     </div>
