7d9932bc438974e1f38ea8f06c0900400043a4c1
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2023-08-07 19:25:54 +0530

    variants, initial, animate, exit and class name were added to the heading, introduction and project button

--- pages/index.js
+++ pages/index.js
@@ -12,24 +12,43 @@ const Home = () => {
       {/* Text */}
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
-          {/* Title */}
-          <h1 className="h1">
+          {/* Heading */}
+          <motion.h1 
+          variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1">
             Hi, I'm Chamil Sachintha <br />{" "}
             <span className="text-accent">
               Full-Stack, Front-End & UI/UX Engineer
             </span>
-          </h1>
-          {/* Subtitle */}
-          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
-            I am passionate about developing full-stack applications, front-end applications and designing UI/UX and have a good point of view on colors.
-          </p>
+          </motion.h1>
+          {/* Introduction */}
+          <motion.p
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+          >
+            I am passionate about developing full-stack applications, front-end
+            applications and designing UI/UX and have a good point of view on
+            colors.
+          </motion.p>
           {/* Button */}
           <div className="flex justify-center xl:hidden relative">
             <ProjectsBtn />
           </div>
-          <div>
+          <motion.div
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden xl:flex"
+          >
             <ProjectsBtn />
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
