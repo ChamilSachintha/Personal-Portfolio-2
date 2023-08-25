d5a985c4856157159fd9246ecae765098846bae5
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2023-08-25 21:40:21 +0530

    Update avatar image

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,8 @@ const Home = () => {
           >
             Hi, I'm Chamil Sachintha <br />{" "}
             <span className="text-accent">
-              Full-Stack, Front-End & UI/UX <br />Engineer
+              Full-Stack, Front-End & UI/UX <br />
+              Engineer
             </span>
           </motion.h1>
           {/* Introduction */}
@@ -54,18 +55,19 @@ const Home = () => {
       </div>
       {/* Image */}
       <div className="w-[1200px] h-full absolute right-0 bottom-0">
-      {/* Background image */}
+        {/* Background image */}
         <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
         {/* Particles */}
         <ParticlesContainer />
         {/* Avatar image */}
         <motion.div
-        variants={fadeIn("up", 0.5)}
-            initial="hidden"
-            animate="show"
-            exit="hidden"
-            translate={{ duration: 1, ease: "easeInOut"}}
-         className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]">
+          variants={fadeIn("up", 0.5)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          translate={{ duration: 1, ease: "easeInOut" }}
+          className="w-full h-full max-w-[510px] max-h-[678px] absolute -bottom-16 lg:right-[10%]"
+        >
           <Avatar />
         </motion.div>
       </div>
