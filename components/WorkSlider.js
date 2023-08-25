f28dc7cf6469174544123c1d143a5803cc876493
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2023-08-25 21:36:23 +0530

    Add work slider

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,51 +1,130 @@
 // data
-const workSlides = {
-  slides: [
-    {
-      images: [
-        {
-          title: 'title',
-          path: '/thumb1.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb2.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb3.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb4.jpg',
-        },
-      ],
-    },
-    {
-      images: [
-        {
-          title: 'title',
-          path: '/thumb4.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb1.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb2.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb3.jpg',
-        },
-      ],
-    },
-  ],
-};
+// const workSlides = {
+//   slides: [
+//     {
+//       images: [
+//         {
+//           title: 'title',
+//           path: '/thumb1.jpg',
+//         },
+//         {
+//           title: 'title',
+//           path: '/thumb2.jpg',
+//         },
+//         {
+//           title: 'title',
+//           path: '/thumb3.jpg',
+//         },
+//         {
+//           title: 'title',
+//           path: '/thumb4.jpg',
+//         },
+//       ],
+//     },
+//     {
+//       images: [
+//         {
+//           title: 'title',
+//           path: '/thumb4.jpg',
+//         },
+//         {
+//           title: 'title',
+//           path: '/thumb1.jpg',
+//         },
+//         {
+//           title: 'title',
+//           path: '/thumb2.jpg',
+//         },
+//         {
+//           title: 'title',
+//           path: '/thumb3.jpg',
+//         },
+//       ],
+//     },
+//   ],
+// };
+
+// Swiper react components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+// Icons
+import { RxArrowTopRight, RxLayout, RxLayers, RxMix } from "react-icons/rx";
+
+// Required modules
+import { FreeMode, Pagination } from "swiper";
+
+// Work data
+const workData = [
+  {
+    icon: <RxLayers />,
+    title: "Full-Stack",
+    description:
+      "Crafting comprehensive solutions that seamlessly integrate front-end and back-end technologies for an end-to-end experience.",
+  },
+  {
+    icon: <RxLayout />,
+    title: "Front-End",
+    description:
+      "Transforming design concepts into interactive and user-friendly interfaces that captivate and engage users.",
+  },
+  {
+    icon: <RxMix />,
+    title: "UI / UX",
+    description:
+      "Elevating digital experiences through intuitive user interfaces and thoughtful user experiences that leave a lasting impact.",
+  },
+];
 
 const WorkSlider = () => {
-  return <div>Work Slider</div>;
+  return (
+    <Swiper
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+        },
+
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+      }}
+      freeMode={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={{ FreeMode, Pagination }}
+      className="h-[240px] sm:h-[340px]"
+    >
+      {workData.map((item, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div className="bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group curser-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300">
+              {/* Icon */}
+              <div className="text-4xl text-accent mb-4">{item.icon}</div>
+              {/* Title and description */}
+              <div className="mb-8">
+                <div className="mb-2 text-lg">{item.title}</div>
+                <p className="max-w-[350px] leading-normal ">
+                  {item.description}
+                </p>
+              </div>
+              {/* Arrow */}
+              <div className="text-3xl">
+                <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-accent transition-all duration-300" />
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default WorkSlider;
+
