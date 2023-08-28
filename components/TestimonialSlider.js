64611df00357d3078b14355bef093eaadf2462c2
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2023-08-28 23:49:39 +0530

    Create testimonial page

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -1,30 +1,145 @@
-// testimonial data
+// Swiper react components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+// Icons
+import { BsArrowRight } from "react-icons/bs";
+
+// Required modules
+import { Pagination } from "swiper";
+
+// Next image
+import Image from "next/image";
+
+// Testimonial data
 const testimonialData = [
   {
-    image: '/t-avt-1.png',
-    name: 'Anne Smith',
-    position: 'Customer',
+    image: "/t-avt-1.png",
+    name: "Anne Smith",
+    position: "Customer",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
   {
-    image: '/t-avt-2.png',
-    name: 'Jane Doe',
-    position: 'Customer',
+    image: "/t-avt-2.png",
+    name: "Jane Doe",
+    position: "Customer",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
   {
-    image: '/t-avt-3.png',
-    name: 'Jhon Doe',
-    position: 'Customer',
+    image: "/t-avt-3.png",
+    name: "Jhon Doe",
+    position: "Customer",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
 ];
 
-const TestimonialSlider = () => {
-  return <div>Testimonial Slider</div>;
+
+
+// Project data
+export const testimonialSlider = {
+  slides: [
+    {
+      images: [
+        {
+          title: "title",
+          path: "/thumb1.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb2.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb3.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb4.jpg",
+        },
+      ],
+    },
+    {
+      images: [
+        {
+          title: "title",
+          path: "/thumb4.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb1.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb2.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb3.jpg",
+        },
+      ],
+    },
+  ],
 };
+
+const TestimonialSlider = () => {
+  return (
+    <Swiper
+      spaceBetween={10}
+      pagination={{
+        clickable: true,
+      }}
+      modules={{ Pagination }}
+      className="h-[280px] sm:h-[480px]"
+    >
+      {testimonialSlider.slides.map((person, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
+              {person.images.map((image, index) => {
+                return (
+                  <div
+                    className="relative rounded-lg overflow-hidden flex items-center justify-center group"
+                    key={index}
+                  >
+                    <div className="flex items-center justify-center relative overflow-hidden group">
+                      {/* Image */}
+                      <Image src={image.path} width={500} height={300} alt="" />
+                      {/* Overlay gradient */}
+                      <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
+                      {/* Title */}
+                      <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
+                        <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
+                          {/* Title part 1 */}
+                          <div className="delay-100">LIVE</div>
+                          {/* Title part 2 */}
+                          <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
+                            PROJECT
+                          </div>
+                          {/* Icon*/}
+                          <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
+                            <BsArrowRight />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
+};
+
+
 
 export default TestimonialSlider;
--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -1,5 +1,21 @@
+// Components
+import TestimonialSlider from "../../components/TestimonialSlider";
+
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+  <div className="h-full bg-primary/30 py-32 text-center">
+    <div className="container mx-auto h-full flex flex-col justify-center">
+      {/* Title */}
+      <h2 className="h2 mb-8 xl:mb-0">
+        Testimonials <span className="text-accent">.</span>
+      </h2>
+      {/* Slider */}
+      <div>
+        <TestimonialSlider />
+      </div>
+    </div>
+  </div>
+  );
 };
 
 export default Testimonials;
