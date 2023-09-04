efd90fddce4cee87ecdaaf191c3f6c0ad72a4914
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2023-09-04 12:09:29 +0530

    Make mobile responsive

--- pages/services/index.js
+++ pages/services/index.js
@@ -11,7 +11,7 @@ const Services = () => {
   return (
     <div className="h-full bg-primary/30 py-36 flex items-center">
       <Circles />
-      <div className="container h-screen overflow-hidden">
+      <div className="container h-screen overflow-hidden w-full">
         <div className="flex flex-col xl:flex-row gap-x-8">
           {/* Text */}
           <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
