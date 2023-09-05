2d2b584c242ee236780b07fd7435364e809eda6d
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2023-09-05 21:20:17 +0530

    Add work page link

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -12,6 +12,9 @@ import { RxArrowTopRight, RxLayout, RxLayers, RxMix } from "react-icons/rx";
 // Required modules
 import { FreeMode, Pagination } from "swiper";
 
+// Link
+import Link from "next/link";
+
 // Service data
 export const serviceData = [
   {
@@ -52,27 +55,29 @@ const ServiceSlider = () => {
       pagination={{
         clickable: true,
       }}
-      modules={[ FreeMode, Pagination ]}
+      modules={[FreeMode, Pagination]}
       className="h-[240px] sm:h-[340px]"
     >
       {serviceData.map((item, index) => {
         return (
           <SwiperSlide key={index}>
-            <div className="bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group curser-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300">
-              {/* Icon */}
-              <div className="text-4xl text-accent mb-4">{item.icon}</div>
-              {/* Title and description */}
-              <div className="mb-8">
-                <div className="mb-2 text-lg">{item.title}</div>
-                <p className="max-w-[350px] leading-normal ">
-                  {item.description}
-                </p>
+            <Link href="/work">
+              <div className="bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group curser-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300">
+                {/* Icon */}
+                <div className="text-4xl text-accent mb-4">{item.icon}</div>
+                {/* Title and description */}
+                <div className="mb-8">
+                  <div className="mb-2 text-lg">{item.title}</div>
+                  <p className="max-w-[350px] leading-normal ">
+                    {item.description}
+                  </p>
+                </div>
+                {/* Arrow */}
+                <div className="text-3xl">
+                  <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-accent transition-all duration-300" />
+                </div>
               </div>
-              {/* Arrow */}
-              <div className="text-3xl">
-                <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-accent transition-all duration-300" />
-              </div>
-            </div>
+            </Link>
           </SwiperSlide>
         );
       })}
